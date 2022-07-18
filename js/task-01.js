794615b7e5b6d0319c7fcfb7a5f767aaefabf6b7
Anastasia-Boroday <anastasiaborodaj22@gmail.com>
2022-07-18 23:23:06 +0300

    add files to hm6

---
+++ js/task-01.js
@@ -0,0 +1,18 @@
+// Number of categories: 3
+
+// Category: Animals
+// Elements: 4
+
+// Category: Products
+// Elements: 3
+
+// Category: Technologies
+// Elements: 5
+
+const itemEl = document.querySelector('.item');
+const itemAllEl = document.querySelectorAll('.item');
+const numberCategories = itemAllEl.length;
+console.log(`Number of categories: ${numberCategories}`);
+
+
+
