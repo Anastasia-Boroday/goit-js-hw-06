41e408bf269a9369dae6ad9c70e469c134c74638
Anastasia-Boroday <anastasiaborodaj22@gmail.com>
2022-07-24 18:09:42 +0300

    corrected task6

--- js/task-06.js
+++ js/task-06.js
@@ -1,11 +1,15 @@
-// query
+// // query
 const inputEl = document.querySelector('#validation-input');
 inputEl.addEventListener('blur', (event) => {
+    
     const arrayInput = event.currentTarget.value.split('');
 
     if (arrayInput.length<6) {
         inputEl.classList.add('invalid');
+        inputEl.classList.remove('valid')
     } else {
         inputEl.classList.add('valid');
+        inputEl.classList.remove('invalid');
     }
 });
+
