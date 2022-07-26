9575022a35d590da4c2ef986accdb57ae473934d
Anastasia-Boroday <anastasiaborodaj22@gmail.com>
2022-07-26 21:30:48 +0300

    change task6

--- js/task-06.js
+++ js/task-06.js
@@ -1,15 +1,18 @@
 // // query
 const inputEl = document.querySelector('#validation-input');
+const lengthEl = document.querySelector('input[data-length="6"]');
+
 inputEl.addEventListener('blur', (event) => {
-    
+    const length = Number(lengthEl.dataset.length);
     const arrayInput = event.currentTarget.value.split('');
 
-    if (arrayInput.length<6) {
-        inputEl.classList.add('invalid');
-        inputEl.classList.remove('valid')
-    } else {
+    if (arrayInput.length === length) {
         inputEl.classList.add('valid');
         inputEl.classList.remove('invalid');
+
+    } else {
+        inputEl.classList.add('invalid');
+        inputEl.classList.remove('valid')
     }
 });
 
